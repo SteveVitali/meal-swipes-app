d7e2b86aed430af22405235f09dc03410d22f802
Steve Vitali <14stevevitali@gmail.com>
2015-01-26 01:14:30 -0500

    Add 'add chef' functionality to admin view

---
+++ client/templates/frat-admin-view.js
@@ -0,0 +1,7 @@
+Template.fratAdminView.events({
+  'click .add-chef': function(event, template) {
+    var chefUsername = template.$('.chef-username-input').val();
+    if (!chefUsername) return;
+    Meteor.call('addChefToFraternity', this._id, chefUsername);
+  }
+})
